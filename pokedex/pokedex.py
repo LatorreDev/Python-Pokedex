4dee98750704962c46141b4ea3a7c66a8ef073a1
David Latorre <dvdn10071856@gmail.com>
2018-12-02 21:38:34 -0500

    add pokedex/pokedex.py for api creation

--- pokedex/pokedex.py
+++ pokedex/pokedex.py
@@ -2,9 +2,11 @@ import click
 
 from pokemons import commands as pokemons_commands
 
+POKEMONS_TABLE = '.pokemons.csv'
 @click.group()
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+    ctx.obj['pokemons_table'] = POKEMONS_TABLE
 
 cli.add_command(pokemons_commands.all)
