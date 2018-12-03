0a2c39711685d17c3f7ea18cb8c78ffb4950fdd8
David Latorre <dvdn10071856@gmail.com>
2018-12-02 21:39:17 -0500

    add commands.py for api creation

--- pokedex/pokemons/commands.py
+++ pokedex/pokemons/commands.py
@@ -1,5 +1,8 @@
 import click
 
+from pokemons.services import PokemonsService
+from pokemons.models import Pokemon
+
 @click.group()
 def pokemons():
     '''Manages the pokemons lifecycle'''
@@ -7,9 +10,55 @@ def pokemons():
 
 
 @pokemons.command()
+@click.option('-n', '--name',
+    type=str,
+    prompt=True,
+    help='The pokemon name')
+
+@click.option('-s', '--species',
+    type=str,
+    prompt=True,
+    help='The pokemon specie')
+
+@click.option('-ta', '--TypeA',
+    type=str,
+    prompt=True,
+    help='The pokemon Type A')
+
+@click.option('-tb', '--TypeB',
+    type=str,
+    prompt=True,
+    help='The pokemon Type B')
+
+@click.option('-w', '--weight',
+    type=int,
+    prompt=True,
+    help='The pokemon weight')
+
+@click.option('-h', '--height',
+    type=int,
+    prompt=True,
+    help='The pokemon height')
+
+@click.option('-a', '--abilities',
+    type=str,
+    prompt=True,
+    help='The pokemon abilities')
+
+@click.option('-d', '--description',
+    type=str,
+    prompt=True,
+    help='The pokemon description')
+
 @click.pass_context
-def create(ctx, name, species, typeA, typeB, weight, height, abilities, description):
+def create(ctx, name, species, typea, typeb, weight, height, abilities, description):
     '''Create a new pokemon'''
+
+    pokemon = Pokemon(name, species, typea, typeb, weight, height, abilities, description)
+    pokemon_service = PokemonsService(ctx.obj['pokemons_table'])
+
+
+    pokemon_service.create_pokemon(pokemon)
 
 
 @pokemons.command()
